284a43eef723966511f7e53d5d2788cbd7c8ef15
Quynh Nguyen <qnguyenx3@gmail.com>
2018-05-05 13:32:46 -0700

    Finish adding font awesome icons

--- app/src/main/java/com/example/android/philz/MainActivity.java
+++ app/src/main/java/com/example/android/philz/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.philz;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView facebookIcon;
+    TextView twitterIcon;
+    TextView instagramIcon;
+    TextView linkedinIcon;
+    TextView pinterestIcon;
+    TextView googleplusIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+
+        facebookIcon = findViewById(R.id.facebook_icon);
+        twitterIcon = findViewById(R.id.twitter_icon);
+        instagramIcon = findViewById(R.id.instagram_icon);
+        linkedinIcon = findViewById(R.id.linkedin_icon);
+        pinterestIcon = findViewById(R.id.pinterest_icon);
+        googleplusIcon = findViewById(R.id.googleplus_icon);
+
+        facebookIcon.setTypeface(iconFont);
+        twitterIcon.setTypeface(iconFont);
+        instagramIcon.setTypeface(iconFont);
+        linkedinIcon.setTypeface(iconFont);
+        pinterestIcon.setTypeface(iconFont);
+        googleplusIcon.setTypeface(iconFont);
+
     }
 }
